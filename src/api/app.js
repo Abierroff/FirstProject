bfd1830c271469eb3ead1cb7d30ffb45604a4204
Abierroff <dboruto3@gmail.com>
2023-11-17 23:19:26 -0300

    ea

--- src/api/app.js
+++ src/api/app.js
@@ -1,12 +1,31 @@
-const app = require("express")()
+const express = require("express")
+const app = express()
+const path = require("node:path")
+
+
 
 
 //crinado a primeira rota
-app.get("/", (req, res) => {
-    res.send("Hello from backend")
+
+app.get("/", (req, res) => { 
+res.send("Welcome to home")
 })
 
+app.get("/user", (req, res) => { 
+    const users = {
+        data: [{
+            name: "abie",
+            age: 20
+        },
+        {
+            name: "test",
+            age: 10
+        }]
 
+    }
+
+    res.json(users.data)
+})
 
 //abrindo o servidor na porta 3000
 app.listen(3000, () => {
